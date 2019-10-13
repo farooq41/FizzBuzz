8b1e9ee0c2167e8f49929f07b6d7c07c964b418b
Mohammed Farooq <mohammed.farooq@ad.infosys.com>
2019-10-14 03:15:03 +0530

    initial

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("Hello, world!");
+            for (int i = 1; i <= 100; i++)
+            {
+                var x = i % 1;
+
+                var print = (i % 3 == 0 || i % 5 == 0) ? (i % 3 == 0 && i % 5 == 0) ? "\t\tFizzBuz" : (i % 3 == 0) ? "\tFizz" : "\tBuzz" : i.ToString();
+                Console.WriteLine("\t\t" + print);
+                Console.WriteLine("\n\r");
+            }
+        }
+    }
+}
